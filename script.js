b3226c0c2660ef299be20939437537c59b5891b4
loreicalla <lorebarley@gmail.com>
2026-08-25 12:51:40 +0800

    Load SEO metadata script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Stable additive loader: preserves existing website logic, then layers guided chatbot and package enhancements. */
 (function(){
   function load(src,next){const s=document.createElement('script');s.src=src;s.onload=next;s.onerror=()=>console.error('Unable to load '+src);document.head.appendChild(s)}
-  load('core.js',()=>load('lore-expert-guide.js',()=>load('package-images.js',()=>load('package-fix.js',()=>{}))));
+  load('seo.js',()=>load('core.js',()=>load('lore-expert-guide.js',()=>load('package-images.js',()=>load('package-fix.js',()=>{})))));
 })();
